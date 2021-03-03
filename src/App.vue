741c956f4eeb3519e60dedeca450fb8f02b2f376
Navruz Halimov <navruz.halimov.3999@gmail.com>
2021-03-03 15:42:29 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Calculator />
+  </div>
+</template>
+<script>
+import Calculator from './components/Calculator'
+export default {
+  name:'app',
+  components:{
+    Calculator
+  }
+}
+</script>
+<style></style>
